c4c88d4b9441947bce7aaebb38e6d888031ad86e
Sokunthaneth Chhoy <chhoysokunthaneth@gmail.com>
2019-02-14 20:13:47 +0700

    Initial commit

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,17 @@
+@extends('user.layout.auth')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    You are logged in as User!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
